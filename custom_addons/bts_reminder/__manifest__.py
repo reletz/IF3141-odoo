d1cc7159ea8d925cd3c1e0f6c0ea360c2bbda128
reletz <naufarrelzhafifabhista@gmail.com>
2026-04-22 14:57:54 +0700

    init: modules

---
+++ custom_addons/bts_reminder/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'BTS Reminder',
+    'version': '17.0.1.0.0',
+    'category': 'Services',
+    'summary': 'Service reminder system',
+    'description': """
+        Basic scaffold for BTS Reminder System.
+    """,
+    'author': 'Your Name',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menus.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ custom_addons/bts_reminder/models/models.py
@@ -0,0 +1,7 @@
+from odoo import models, fields, api
+
+class BtsReminder(models.Model):
+    _name = 'bts.reminder'
+    _description = 'BTS Reminder'
+
+    # Add fields here
